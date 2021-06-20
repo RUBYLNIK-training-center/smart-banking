26665ca08c71cbea1f2e5c97735e38f9797091f9
vorob <vorobyov_egor@bk.ru>
2021-06-20 21:55:16 +0300

    Fix

--- app/models/service.rb
+++ app/models/service.rb
@@ -2,5 +2,5 @@ class Service < ApplicationRecord
   validates :name, presence: true, length: { minimum: 2, maximum: 20 }
 
   belongs_to :wallet
-  belongs_to :category, dependent: :nullify
+  belongs_to :category
 end
