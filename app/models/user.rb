851619ca741c72c425a564a646340fadcade50f9
m3xq <z3w1l0q@protonmail.com>
2021-06-20 10:01:17 -0500

    Add auth with google

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,24 @@
 class User < ApplicationRecord
-  validates :name, presence: true, length: { minimum: 2, maximum: 20 }
-  validates :age, presence: true, numericality: { greater_than_or_equal_to: 0, only_integer: true }
-
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  devise :omniauthable, omniauth_providers: [:google_oauth2]
+
+  validates :name, presence: true, length: { minimum: 2, maximum: 20 }
+  validates :age, presence: true, numericality: { greater_than_or_equal_to: 0, only_integer: true }
 
   validates_associated :wallets
 
   has_many :wallets, dependent: :destroy
   has_many :subscriptions, dependent: :destroy
+
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.find_by(email: data['email'])
+
+    user || User.create(name: data['first_name'],
+                        surname: data['last_name'],
+                        email: data['email'],
+                        password: Devise.friendly_token[0, 20])
+  end
 end
