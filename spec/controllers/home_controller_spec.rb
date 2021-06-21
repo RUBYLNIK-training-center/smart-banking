fec2edfbd5820a52b9cc8b3fbda17fd19ae719ee
vorob <vorobyov_egor@bk.ru>
2021-06-21 20:26:30 +0300

    Add user page

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -9,13 +9,5 @@ RSpec.describe HomeController, type: :controller do
         expect(subject.current_user).not_to eq(nil)
       end
     end
-
-    context 'when user is not logged in' do
-      login_user
-
-      it 'is expected not to have a current_user' do
-        expect(subject.current_user).to eq(nil)
-      end
-    end
   end
 end
