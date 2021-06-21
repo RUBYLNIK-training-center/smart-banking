c21efed9d0067e9a422c1f63720d1c45946548a1
m3xq <z3w1l0q@protonmail.com>
2021-06-21 06:49:31 -0500

    Fix Metrics/AbcSize

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  def google_oauth2
+  def google_oauth2 # rubocop:disable Metrics/AbcSize
     @user = User.from_omniauth(request.env['omniauth.auth'])
 
     if @user.persisted?
