e7c7e6a10b704068612a93f4fec8c4ed5368a1dd
vorob <vorobyov_egor@bk.ru>
2021-06-21 18:24:48 +0300

    Deleted useless assignment to variable

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: %i[update]
+
+  def show
+    @user = set_user
+  end
+
+  def edit; end
+
+  def update
+    @user = set_user
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'Your account was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def set_user
+    User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :surname, :age, :email, :avatar, :phone_number, :passport_number, :password,
+                                 :password_confirmation)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/update'
   devise_for :users, controllers: { 
     registrations: 'users/registrations', 
     omniauth_callbacks: 'users/omniauth_callbacks' 
   }
 
+  resources :users
+  
   get 'home/index'
+  get '/about', to: 'home#about'
   root 'home#index'
 end
