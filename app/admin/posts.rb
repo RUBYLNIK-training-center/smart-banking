c18ff58be1aadc7181666d5798b070bc5dbc68f9
vorob <vorobyov_egor@bk.ru>
2021-06-25 00:48:46 +0300

    Pin the current admin to the post creation page

--- app/admin/posts.rb
+++ app/admin/posts.rb
@@ -1,18 +1,17 @@
 ActiveAdmin.register Post do
+  form do |f|
+    f.label "Author id is #{current_admin_user.id}", class: 'label'
+    f.inputs do
+      f.input :name
+      f.input :description
+    end
+    f.submit "Create post", class: "btn btn-primary"
+  end
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  permit_params :admin_user_id, :name, :description
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:admin_user_id, :name, :description]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  member_action :create, method: :post do
+    post = Post.new(name: params[:post][:name], description: params[:post][:description])
+    post.admin_user_id = current_admin_user.id
+    post.save
+    redirect_to admin_post_path(post)
+  end
 end
