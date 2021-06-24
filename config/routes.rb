95282cd5a06b597f02ebd17cd00687250633d355
vitalygv <kewl.dkmr@gmail.com>
2021-06-24 18:34:15 +0300

    Correct routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users, only: :omniauth_callbacks, controllers: {omniauth_callbacks: 'users/omniauth_callbacks'}
+  ActiveAdmin.routes(self)
+  devise_for :admin_users, ActiveAdmin::Devise.config
 
   scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
-    devise_for :admin_users, ActiveAdmin::Devise.config
     devise_for :users, skip: :omniauth_callbacks, controllers: { 
       registrations: 'users/registrations'
     }
 
     resources :users
   
-    ActiveAdmin.routes(self)
-
     get 'home/index'
     get '/about', to: 'home#about'
     resources :post, only: :show
