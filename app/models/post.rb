15fa006fca36f20773272935d2fbca9a1514d7cb
Vorobyov Egor <43318298+Captain-Vorobey@users.noreply.github.com>
2021-06-25 15:11:42 +0300

    Update post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,6 @@ class Post < ApplicationRecord
   belongs_to :admin_user, class_name: 'AdminUser'
 
   validates :name, presence: true, length: { minimum: 2 }
-  validates :description, presence: true, length: { minimum: 20, maximum: 2000 }
+  validates :description, presence: true, length: { maximum: 2000 }
   paginates_per 5
 end
