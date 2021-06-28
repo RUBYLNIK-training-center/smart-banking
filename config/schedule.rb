2063e20dd67559d9f88de6ff11fc98f042118bca
m3xq <z3w1l0q@protonmail.com>
2021-06-28 16:38:02 -0500

    Fix config/schedule.rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,7 @@
 env :PATH, ENV['PATH']
 
+job_type :rake,    "cd :path && :environment_variable=:environment bundle exec /home/deploy/.rbenv/shims/rake  :task --silent :output"
+
 every 1.hour do
     rake 'rates:parse'    
 end
