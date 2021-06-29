41a2f6a416b2722f849e73bd8c393a7ef52985e3
vitalygv <kewl.dkmr@gmail.com>
2021-06-30 00:46:58 +0300

    Remove redundant methods in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,18 +6,11 @@ class UsersController < ApplicationController
 
   def edit
     @user = user
-    @user.avatar.attach(params[:avatar])
+    # @user.avatar.attach(params[:avatar])
   end
 
   def update
     @user = user
-    # if @user.avatar.attached?
-    #   @user.avatar.purge
-    #   @user.avatar.attach(params[:avatar])
-    # else
-    #   @user.avatar.attach(params[:avatar])
-    # end
-
     respond_to do |format|
       if @user.update(user_params)
         format.html { redirect_to @user, notice: 'Your account was successfully updated.' }
