e83330f3d9020576bfcc2797b1bfe582146fc572
lizamuromskaya <lizavetamuromskaya@gmail.com>
2021-07-01 21:03:57 +0300

    [SB-38] Add a user picture to the Profile button in the navbar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_26_132148) do
+ActiveRecord::Schema.define(version: 2021_06_27_192758) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -152,12 +152,11 @@ ActiveRecord::Schema.define(version: 2021_06_26_132148) do
   create_table "wallets", force: :cascade do |t|
     t.bigint "currency_id", null: false
     t.decimal "amount"
-    t.string "iban"
-    t.integer "wallet_number"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
     t.boolean "locked"
+    t.bigint "wallet_number"
     t.index ["currency_id"], name: "index_wallets_on_currency_id"
     t.index ["user_id"], name: "index_wallets_on_user_id"
   end
