b1ff744a9525a55a55624a5500fe548b564f0c71
vorob <vorobyov_egor@bk.ru>
2021-07-02 16:31:48 +0300

    Add user security

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,33 +2,8 @@ class UsersController < ApplicationController
   include WalletsHelpers
 
   def show
-    @user = user
-    @wallets = WalletsHelpers.unlocked_user_wallets(@user)
+    @wallets = WalletsHelpers.unlocked_user_wallets(current_user)
     @currencies = Currency.order(:name)
     @wallet = Wallet.new
-  end
-
-  def edit
-    @user = user
-  end
-
-  def update
-    @user = user
-      if @user.update(user_params)
-        redirect_to @user, notice: 'Your account was successfully updated.'
-      else
-        redirect_to request.referer, notice: @user.errors.full_messages
-      end
-  end
-
-  private
-
-  def user
-    @user ||= User.find(params[:id])
-  end
-
-  def user_params
-    params.require(:user).permit(:name, :surname, :age, :email, :avatar, :phone_number, :passport_number, :password,
-                                 :password_confirmation)
   end
 end
