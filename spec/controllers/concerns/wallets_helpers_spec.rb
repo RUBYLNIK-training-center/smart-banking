e5fc3498620e658b788b00dc785774b39b7b63ae
vorob <vorobyov_egor@bk.ru>
2021-07-02 10:45:10 +0300

    Remove comments

--- spec/controllers/concerns/wallets_helpers_spec.rb
+++ spec/controllers/concerns/wallets_helpers_spec.rb
@@ -1,17 +1,5 @@
 require 'rails_helper'
 
-# def create_random_wallet_number
-# rand(1_111_111_111_111_111..9_999_999_999_999_999)
-# end
-#
-# def currencies
-# Currency.all
-# end
-#
-# def unlocked_user_wallets(user)
-# Wallet.where(user: user, locked: false)
-# end
-
 RSpec.describe WalletsHelpers, type: :concern do
   include WalletsHelpers
 
