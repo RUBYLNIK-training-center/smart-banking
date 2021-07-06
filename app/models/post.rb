2891aca45240c32196242a3eaee2982e3082e9df
Egor <vorobyov_egor!@bk.ru>
2021-07-06 19:53:41 +0300

    Fix post creation bug

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,8 +9,7 @@ class Post < ApplicationRecord
   validates :description, presence: true, length: { maximum: 2000 }
   validate :avatar_file_size, :avatar_file_format
 
-  validates :avatar, attached: true, content_type: ['image/png', 'image/jpeg', 'image/jpg'],
-                     dimension: { width: 150, height: 150 }
+  validates :avatar, content_type: ['image/png', 'image/jpeg', 'image/jpg']
 
   paginates_per 5
 end
