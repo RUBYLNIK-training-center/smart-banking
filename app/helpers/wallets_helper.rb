132ffa012ffc2949aaac4379816d72a670c6323d
lizamuromskaya <lizavetamuromskaya@gmail.com>
2021-07-08 23:45:20 +0300

    added method

--- app/helpers/wallets_helper.rb
+++ app/helpers/wallets_helper.rb
@@ -3,4 +3,7 @@ module WalletsHelper
     wallet_number = number.to_s
     "#{wallet_number[0..3]} - #{wallet_number[4..7]} - #{wallet_number[8..11]} - #{wallet_number[11..14]}"
   end
+  def user_fields_exist?
+    current_user.name.present? && current_user.surname.present?  && current_user.passport_number.present?
+  end
 end
