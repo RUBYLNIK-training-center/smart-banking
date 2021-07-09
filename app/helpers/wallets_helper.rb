2489607316dad19e9e478b7ba0dd924daf58cc4f
Egor <vorobyov_egor!@bk.ru>
2021-07-09 12:31:14 +0300

    Fix wallet view

--- app/helpers/wallets_helper.rb
+++ app/helpers/wallets_helper.rb
@@ -1,6 +1,6 @@
 module WalletsHelper
   def parse_wallet_number(number)
     wallet_number = number.to_s
-    "#{wallet_number[0..3]} - #{wallet_number[4..7]} - #{wallet_number[8..11]} - #{wallet_number[11..14]}"
+    "#{wallet_number[0..3]} - #{wallet_number[4..7]} - #{wallet_number[8..11]} - #{wallet_number[12..15]}"
   end
 end
