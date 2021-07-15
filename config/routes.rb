77a01fdb5791aa26690345c5e352a6248e5926d7
vitalygv <kewl.dkmr@gmail.com>
2021-07-15 22:22:02 +0300

    Add pdf receipts generation

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
     
     resources :wallets
     resource :users
-    resource :transactions
+    resources :transactions
     resources :categories
     
     resources :services
