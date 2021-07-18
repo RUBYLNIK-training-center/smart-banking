843289f9c1a0f02c67ee29f9ab69c59d5b4d2eb9
Egor <vorobyov_egor!@bk.ru>
2021-07-18 21:53:04 +0300

    Add sum validation

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -4,6 +4,8 @@ class Transaction < ApplicationRecord
   belongs_to :user
 
   validates :date, presence: true, date: true
+  validates :sum, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :fee, presence: true, numericality: { greater_than_or_equal_to: 0 }
 
   default_scope { order(created_at: :desc) }
 
